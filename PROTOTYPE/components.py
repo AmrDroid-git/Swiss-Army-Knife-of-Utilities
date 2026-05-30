1f486e1aa575c90610ad47540a113a48f82b2887
AmrSlama <amrdroidcommunity@gmail.com>
2026-05-30 11:07:53 +0100

    Save current work

--- PROTOTYPE/components.py
+++
@@ -1,6 +0,0 @@
-# This file now just acts as a bridge to the smaller files!
-
-from elements.base import BaseComponent
-from elements.script_button import ScriptButton
-from elements.custom_field import CustomField
-from elements.draggable_label import DraggableLabel
--- PROTOTYPE/elements/__init__.py
+++
@@ -1,6 +0,0 @@
-from .base import BaseComponent
-from .script_button import ScriptButton
-from .custom_field import CustomField
-from .draggable_label import DraggableLabel
-
-# By importing them here, the 'components' folder acts exactly like your old components.py file!
